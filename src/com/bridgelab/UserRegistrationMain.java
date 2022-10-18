df2f72b28d21a6205a34f1ed13a54c588f1e4598
nitis <nitish8152@gmail.com>
2022-10-18 18:37:25 +0530

    Added: Regex Program

---
+++ src/com/bridgelab/UserRegistrationMain.java
@@ -0,0 +1,7 @@
+package com.bridgelab;
+
+public class UserRegistrationMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome User Registration Program");
+    }
+}
