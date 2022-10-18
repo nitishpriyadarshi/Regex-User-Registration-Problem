51c5933f33969c25c71e5b3b66cbd586ac5e537a
nitis <nitish8152@gmail.com>
2022-10-18 18:59:52 +0530

    Added: User Need to Enter a Valid Email id

--- src/com/bridgelab/UserRegistrationMain.java
+++ src/com/bridgelab/UserRegistrationMain.java
@@ -7,6 +7,7 @@ public class UserRegistrationMain {
             // Method calling
             validateUserInfo.checkFirstName();
             validateUserInfo.checkLastName();
+            validateUserInfo.checkEmail();
         }
     }
 
